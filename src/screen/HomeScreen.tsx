0467548d969f31d6d6ff6efc5b744671f6d35862
Dima Tretyak <itretyak@ya.ru>
2021-11-09 20:42:53 +0300

    chore: fix typo

--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -65,7 +65,7 @@ export function HomeTabs({ navigation }: HomeScreenProps) {
 						}}
 					/>
 					<Button
-						title="Go to Cheking"
+						title="Go to Checking"
 						onPress={() => {
 							navigation.navigate('Checking', { textParam: 'main account(2932)' });
 						}}
