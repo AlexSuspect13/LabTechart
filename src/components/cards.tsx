c75947bd67c1ba23446679ffab3ebd90de525a3f
AlexSuspect13 <pmodelist@bk.ru>
2021-11-28 13:36:26 +0300

    fix text style on android

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -1,82 +1,81 @@
-import React from "react";
-import { View, Text, StyleSheet,Image } from "react-native"
-import { Button} from "react-native-elements";
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { Button } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+export const Card = ({ kidsImg }) => {
+	return (
+		<View style={styles.container}>
+			<View style={styles.headerContainer}>
+				<Image source={require('../../assets/img/avatar.png')} />
+				<View style={styles.infoAboutPost}>
+					<Text style={styles.text}> Yuor Giving Impact</Text>
+					<View style={styles.dateName}>
+						<Text style={styles.text}> St Jude</Text>
+						<View style={styles.point} />
+						<Text style={styles.text}> 4 hrs Ago</Text>
+					</View>
+				</View>
+			</View>
+			<Image style={styles.kidsPhoto} source={kidsImg}></Image>
+			<Text style={styles.textAboutKids}>
+				Danny, Your donation helped 5 amazing kids get much needed cancer sergery, thanks for being amazing!{' '}
+			</Text>
 
-
-
-
-
-export const Card = ({kidsImg})=>{
-  return(
-    <View style = {styles.container}>
-    <View style = {styles.headerContainer}>
-        <Image source={require("../../assets/img/avatar.png")}/>
-        <View style = {styles.infoAboutPost}>
-            <Text> Yuor Giving Impact</Text>
-            <View style={styles.dateName}>
-              <Text> St Jude</Text>
-              <View style={styles.point}/>
-              <Text> 4 hrs Ago</Text>
-            </View>
-        </View>
-    </View>
-    <Image style={styles.kidsPhoto} source={kidsImg}></Image>
-    <Text style={styles.textAboutKids}>Danny, Your donation helped 5 amazing kids get much needed cancer sergery, thanks for being amazing! </Text>
-  
-      <Button title={'Share to spread the word'} buttonStyle={styles.cardButton} icon={
-       <Icon color={'#fff'} size={13} name = 'share'/>
-       }/>
-       
-
-  </View>
-  )
-  
-}
-
-
+			<Button
+				title={'Share to spread the word'}
+				buttonStyle={styles.cardButton}
+				icon={<Icon color={'#fff'} size={13} name="share" />}
+			/>
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-  container:{
-    display: 'flex',
-    borderWidth: 1,
-    backgroundColor: 'white',
-    color: 'black',
-    borderRadius: 7,
-    borderColor: "#DCDCDC",
-    margin: 15
-  },
-  headerContainer:{
-    flexDirection: 'row',
-    padding: 10,
-},
-infoAboutPost:{
-  flexDirection: 'column',
-  alignItems: 'flex-start',
-  justifyContent: 'center'
-},
-kidsPhoto:{
-width: '100%'
-},
+	container: {
+		display: 'flex',
+		borderWidth: 1,
+		backgroundColor: 'white',
+		color: 'black',
+		borderRadius: 7,
+		borderColor: '#DCDCDC',
+		margin: 15,
+	},
+	headerContainer: {
+		flexDirection: 'row',
+		padding: 10,
+	},
+	infoAboutPost: {
+		flexDirection: 'column',
+		alignItems: 'flex-start',
+		justifyContent: 'center',
+	},
+	kidsPhoto: {
+		width: '100%',
+	},
 
-textAboutKids:{
-padding: 10
-},
-point:{
-  height:5,
-  width:5,
-  backgroundColor: "#ff1493",
-  borderRadius: 40,
-
-},
-dateName:{
-  flexDirection: 'row'
-},
-cardButton:{
-fontSize: 10,
-margin: 20,
-backgroundColor: '#ff1493',
-borderRadius: 20
-}
-})
+	text: {
+		color: '#000',
+	},
+	textAboutKids: {
+		padding: 10,
+		color: '#000',
+	},
+	point: {
+		height: 5,
+		width: 5,
+		backgroundColor: '#ff1493',
+		borderRadius: 40,
+		alignSelf: 'center',
+		margin: 5,
+	},
+	dateName: {
+		flexDirection: 'row',
+	},
+	cardButton: {
+		fontSize: 10,
+		margin: 20,
+		backgroundColor: '#ff1493',
+		borderRadius: 20,
+	},
+});
--- src/components/infoBloks.tsx
+++ src/components/infoBloks.tsx
@@ -1,55 +1,56 @@
-import React from "react";
-import {PanResponder, StyleSheet, Text, View} from 'react-native'
+import React from 'react';
+import { PanResponder, StyleSheet, Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-
-export const InfoBlock =({text,infotxt,dollar,cent})=>{
-  return(
-    <View style={styles.infoBlock}>
-        <View style={styles.infoAbtPage}>
-        <Text style={styles.namePage}>{text}</Text>
-        <Text style = {styles.info}>{infotxt}</Text>
-        </View>
-        <View style={styles.Accountinf}>
-          <Icon size={20} name ="dollar"/>
-          <Text style={styles.infAbtAccount}>{dollar}<Text style={{fontSize: 16}}>.{cent}</Text></Text>
-          <Icon style={styles.chevron}  color={"#ff1493"} name ={"chevron-right"}/>
-        </View>
-    </View>
-  );
-}
-
+export const InfoBlock = ({ text, infotxt, dollar, cent }) => {
+	return (
+		<View style={styles.infoBlock}>
+			<View style={styles.infoAbtPage}>
+				<Text style={styles.namePage}>{text}</Text>
+				<Text style={styles.info}>{infotxt}</Text>
+			</View>
+			<View style={styles.Accountinf}>
+				<Icon size={20} name="dollar" />
+				<Text style={styles.infAbtAccount}>
+					{dollar}
+					<Text style={{ fontSize: 16, color: '#000' }}>.{cent}</Text>
+				</Text>
+				<Icon style={styles.chevron} color={'#ff1493'} name={'chevron-right'} />
+			</View>
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-  infoBlock:{
-    
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    padding: 10
-  }, 
-  infoAbtPage:{
-    flexDirection: 'column'
-  },
-  info:{
-    color: "#c0c0c0"
-  },
-  infAbtAccount:{
-    fontSize: 20,
-    textAlign: 'right'
-  },
-  namePage:{
-    fontSize: 20
-  },
-  Accountinf:{
-    flex: 1,
-    padding: 15,
-    flexDirection: 'row',
-    justifyContent:'flex-end',
-    
-  },
-  chevron:{
-    alignSelf: 'center',
-    margin: 5
-  }
-})
+	infoBlock: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'flex-start',
+		padding: 10,
+	},
+	infoAbtPage: {
+		flexDirection: 'column',
+	},
+	info: {
+		color: '#c0c0c0',
+	},
+	infAbtAccount: {
+		fontSize: 20,
+		textAlign: 'right',
+		color: '#000',
+	},
+	namePage: {
+		fontSize: 20,
+		color: '#000',
+	},
+	Accountinf: {
+		flex: 1,
+		padding: 15,
+		flexDirection: 'row',
+		justifyContent: 'flex-end',
+	},
+	chevron: {
+		alignSelf: 'center',
+		margin: 5,
+	},
+});
