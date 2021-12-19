123ae2cfb27350e3b79e1fee6e5e5adba64a4bc5
AlexSuspect13 <pmodelist@bk.ru>
2021-12-19 18:41:39 +0300

    add style for change avatar

--- src/screen/ProfileScreen.tsx
+++ src/screen/ProfileScreen.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Header } from '../components/Header';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../Redux/store';
 import DatePicker from 'react-native-date-picker';
+import { Button } from 'react-native-elements';
 
 export const Profile = () => {
 	const [date, setDate] = React.useState(new Date());
@@ -16,23 +17,37 @@ export const Profile = () => {
 	return (
 		<SafeAreaView style={styles.container} edges={['left', 'right']}>
 			<Header title="Profile" />
+			<View style={styles.avatarEdit}>
+				<Image style={styles.profilePhoto} source={require('../../assets/img/standartProfilePhoto.png')}></Image>
+				<View style={styles.profileButton}>
+					<Button buttonStyle={styles.button} title="Make from camera" onPress={() => setOpen(true)} />
+					<Button buttonStyle={styles.button} title="Choose from Galery" onPress={() => setOpen(true)} />
+				</View>
+			</View>
 			<View style={styles.item1}>
 				<Text style={styles.fullName}>Full Name </Text>
-				<TextInput placeholder={name} placeholderTextColor="#c0c0c0" onChangeText={setNickName} />
+				<TextInput
+					style={styles.infoText}
+					placeholder={name}
+					placeholderTextColor="#c0c0c0"
+					onChangeText={setNickName}
+				/>
 				<View style={styles.lineUnderInput} />
 
 				<Text style={styles.fullName}>Birthday </Text>
-				<Text>{birthday}</Text>
+				<Text style={styles.infoText}>{birthday}</Text>
 				<View style={styles.lineUnderInput} />
 
-				<Button title="Change birthday" onPress={() => setOpen(true)} />
+				<Button buttonStyle={styles.button} title="Change birthday" onPress={() => setOpen(true)} />
 				<Button
+					buttonStyle={styles.button}
 					title="Change name"
 					onPress={() => {
 						dispatch({ type: 'RENAME_USER', Username: nickName });
 					}}
 				/>
 				<Button
+					buttonStyle={styles.button}
 					title="Exit"
 					onPress={() => {
 						dispatch({ type: 'SIGN_OUT', token: null });
@@ -61,13 +76,32 @@ export const Profile = () => {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		backgroundColor: '#fff',
 	},
 	item1: {
 		flex: 1,
-		backgroundColor: '#fff',
+
 		padding: 20,
 	},
 	fullName: { color: '#636363', fontSize: 20, fontFamily: 'SFRounded-Medium' },
 	lineUnderInput: { height: 1, backgroundColor: '#636363', marginBottom: 30 },
 	birthday: { flexDirection: 'row' },
+	infoText: { color: 'black' },
+	button: {
+		marginBottom: 15,
+		backgroundColor: '#ff1493',
+		borderRadius: 15,
+	},
+	avatarEdit: {
+		padding: 20,
+	},
+	profilePhoto: {
+		alignSelf: 'center',
+	},
+	profileButton: {
+		marginHorizontal: 10,
+		top: 30,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
 });
